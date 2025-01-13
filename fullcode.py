2809145767b7a5dfb715eef63099c410ea3a7da5
freedomwebtech <48402744+freedomwebtech@users.noreply.github.com>
2025-01-13 13:39:38 +0530

    Add files via upload

---
+++ fullcode.py
@@ -0,0 +1,62 @@
+import cv2
+import numpy as np
+from ultralytics import YOLO
+import cvzone
+
+
+
+
+
+
+# Load YOLO11 model
+model = YOLO("best.pt")
+names = model.names
+
+
+
+# Open the video file or webcam
+cap = cv2.VideoCapture('6.mp4')
+count=0
+
+while True:
+    # Read a frame from the video
+    ret, frame = cap.read()
+    
+    if not ret:
+        break
+    
+    frame = cv2.resize(frame, (640, 500))
+    
+    # Run YOLO11 tracking on the frame
+    results = model.track(frame, persist=True)
+    
+  
+    
+    # Check if there are any boxes in the results
+    if results[0].boxes is not None and results[0].boxes.id is not None:
+        # Get the boxes, class IDs, track IDs, and confidences
+        boxes = results[0].boxes.xyxy.int().cpu().tolist()  # Bounding boxes
+        class_ids = results[0].boxes.cls.int().cpu().tolist()  # Class IDs
+        track_ids = results[0].boxes.id.int().cpu().tolist()  # Track IDs
+        confidences = results[0].boxes.conf.cpu().tolist()  # Confidence scores
+        
+        for box, class_id, track_id, conf in zip(boxes, class_ids, track_ids, confidences):
+            c = names[class_id]
+            x1,y1,x2,y2=box
+            cv2.rectangle(frame,(x1,y1),(x2,y2),(0,0,255),2)
+            cvzone.putTextRect(frame,f'{c}',(x1,y1),1,1)
+            
+    
+    
+
+#    cv2.polylines(frame,[np.array(area1,np.int32)],True,(255,0,255),2)
+#    cv2.polylines(frame,[np.array(area2,np.int32)],True,(255,0,255),2)
+
+    # Display the frame
+    cv2.imshow("FRAME", frame)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+# Release the video capture object and close the display window
+cap.release()
+cv2.destroyAllWindows()
